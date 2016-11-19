fc514d44b14c6a2ee6e02834ab54ce649c3810bd
MegaByteMe <code_monkey@comcast.net>
2016-11-18 22:15:00 -0500

    GoL - added flipAbit method UI - added ability to click cells to bring them to life and add them to grid.

--- gol/Form1.Designer.cs
+++ gol/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.step_Button = new System.Windows.Forms.Button();
+            this.run_Button = new System.Windows.Forms.Button();
+            this.clear_Button = new System.Windows.Forms.Button();
+            this.stop_Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // step_Button
+            // 
+            this.step_Button.Location = new System.Drawing.Point(283, 380);
+            this.step_Button.Name = "step_Button";
+            this.step_Button.Size = new System.Drawing.Size(75, 23);
+            this.step_Button.TabIndex = 0;
+            this.step_Button.Text = "Step";
+            this.step_Button.UseVisualStyleBackColor = true;
+            this.step_Button.Click += new System.EventHandler(this.step_Button_Click);
+            // 
+            // run_Button
+            // 
+            this.run_Button.Location = new System.Drawing.Point(364, 380);
+            this.run_Button.Name = "run_Button";
+            this.run_Button.Size = new System.Drawing.Size(75, 23);
+            this.run_Button.TabIndex = 1;
+            this.run_Button.Text = "Run";
+            this.run_Button.UseVisualStyleBackColor = true;
+            this.run_Button.Click += new System.EventHandler(this.run_Button_Click);
+            // 
+            // clear_Button
+            // 
+            this.clear_Button.Location = new System.Drawing.Point(445, 380);
+            this.clear_Button.Name = "clear_Button";
+            this.clear_Button.Size = new System.Drawing.Size(75, 23);
+            this.clear_Button.TabIndex = 2;
+            this.clear_Button.Text = "Clear";
+            this.clear_Button.UseVisualStyleBackColor = true;
+            this.clear_Button.Click += new System.EventHandler(this.clear_Button_Click);
+            // 
+            // stop_Button
+            // 
+            this.stop_Button.Location = new System.Drawing.Point(526, 380);
+            this.stop_Button.Name = "stop_Button";
+            this.stop_Button.Size = new System.Drawing.Size(75, 23);
+            this.stop_Button.TabIndex = 3;
+            this.stop_Button.Text = "Stop";
+            this.stop_Button.UseVisualStyleBackColor = true;
+            this.stop_Button.Click += new System.EventHandler(this.stop_Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 261);
+            this.ClientSize = new System.Drawing.Size(871, 415);
+            this.Controls.Add(this.stop_Button);
+            this.Controls.Add(this.clear_Button);
+            this.Controls.Add(this.run_Button);
+            this.Controls.Add(this.step_Button);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,5 +90,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button step_Button;
+        private System.Windows.Forms.Button run_Button;
+        private System.Windows.Forms.Button clear_Button;
+        private System.Windows.Forms.Button stop_Button;
     }
 }
--- gol/Form1.cs
+++ gol/Form1.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// ------------------------------------------------------------ >>
+// Project: GoL
+//
+// TODO: 
+//
+// Good: 
+//
+// |==> Call placePattern before a tick() call to ensure changes are applied to grid
+//      
+// ------------------------------------------------------------ >>
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +22,128 @@ namespace gol
 {
     public partial class Form1 : Form
     {
+        // Hang Global References
+        Button[,] MCB;
+        GameOfLife gold;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Define game grid size
+            const int rows = 24;
+            const int columns = 56;
+            bool[,] demo = new bool[rows, columns];
+
+            // Instantiate GameofLife object
+            gold = new GameOfLife(demo, rows, columns);
+
+            // Place some patterns
+            gold.placePattern(GameOfLife.patterns.beacon, 7, 7);
+            gold.placePattern(GameOfLife.patterns.blinker, 8, 2);
+            gold.placePattern(GameOfLife.patterns.block, 1, 5);
+            gold.placePattern(GameOfLife.patterns.toad, 2, 10);
+
+            Point p = new Point(0, 0);
+            Size s = new Size(12, 12);
+            MCB = new Button[rows, columns];
+            int findy = 0;
+
+            // Init Button Array - Visual Representation of Grid
+            for (int i = 0; i < rows; i++)
+            {
+                p.X = 2;
+                p.Y += 14;
+                for (int j = 0; j < columns; j++)
+                {
+                    p.X += 14;
+
+                    Button b = new Button();
+                    b.Size = s;
+                    b.Location = p;
+                    b.BackColor = Color.Red;
+                    MCB[i, j] = b;
+                    this.Controls.Add(b);
+                    b.Name = findy.ToString();
+                    findy++;
+                    b.Click += new EventHandler(findmy_Button_Click);
+                }
+            }
+            this.Update();
         }
-    }
+
+        private void step_Button_Click(object sender, EventArgs e)
+        {
+            gold.tick();
+            drawGrid();
+            this.Update();
+        } // End step_Button_Click
+
+        private void run_Button_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 60; j++)
+            {
+                gold.tick();
+                drawGrid();
+                this.Update();
+
+                for (int i = 0; i < int.MaxValue / 20; i++)
+                {
+                    // Burn some time
+                }
+            }
+        } // End run_Button_Click
+
+        private void clear_Button_Click(object sender, EventArgs e)
+        {
+            int row = gold.ROWS, col = gold.COLUMNS;
+            bool[,] boo = new bool[row, col];
+            gold = new GameOfLife(boo, row, col);
+            drawGrid();
+        } // End clear_Button_Click
+
+        private void stop_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        } // End stop_Button_Click
+
+        private void findmy_Button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.BackColor = Color.AliceBlue;
+            int temp = 0;
+            int.TryParse(b.Name, out temp);
+            if (gold.COLUMNS - temp >= 0)
+            {
+                gold.flipAbit(0, temp);
+            }
+            else
+            {
+                int j = gold.COLUMNS + gold.COLUMNS;
+                for (int i = 1; i < gold.ROWS; i++)
+                {
+                    if (j - temp >= 0)
+                    {
+                        gold.flipAbit(i, temp - gold.COLUMNS*i );
+                        break;
+                    }
+                    else j += gold.COLUMNS;
+                }
+            }
+            drawGrid();
+        }
+
+        private void drawGrid()
+        {
+            for (int i = 0; i < gold.ROWS; i++)
+            {
+                for (int j = 0; j < gold.COLUMNS; j++)
+                {
+                    if (gold.Grid[i, j] == true) MCB[i, j].BackColor = Color.Green;
+                    else MCB[i, j].BackColor = Color.White;
+                }
+            }
+        } // end drawGrid method
+
+    } // End form1
 }
