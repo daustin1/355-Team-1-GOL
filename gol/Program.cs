0e513f74710899908aa78b1f2d63f15ae3832a1b
MegaByteMe <code_monkey@comcast.net>
2016-11-18 21:03:19 -0500

    Implemented crude UI

--- gol/Program.cs
+++ gol/Program.cs
@@ -8,12 +8,28 @@
 //      
 // ------------------------------------------------------------ >>
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace gol
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        static void woo()
         {
             // Define game grid size
             const int rows = 12;
